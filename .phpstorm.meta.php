3d7957340c7375190d02ce115a06d3a686fdda12
munazzam <munazzamm@gmail.com>
2018-11-27 20:16:39 -0500

    db updates

--- .phpstorm.meta.php
+++ .phpstorm.meta.php
@@ -5,7 +5,7 @@ namespace PHPSTORM_META {
 
    /**
     * PhpStorm Meta file, to provide autocomplete information for PhpStorm
-    * Generated on 2018-11-23 07:37:22.
+    * Generated on 2018-11-28 01:02:52.
     *
     * @author Barry vd. Heuvel <barryvdh@gmail.com>
     * @see https://github.com/barryvdh/laravel-ide-helper
